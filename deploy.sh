8451fc67cffe8c6ef236769da60e4446d74e0d0e
Anuj Sharma <anujjangra25119@gmail.com>
2019-02-27 17:39:13 +0530

    flushed files

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,15 @@
-sudo snap install kubectl --classic
-
-export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-sudo apt-get update && sudo apt-get install google-cloud-sdk
-
-
-wget https://github.com/kubernetes/kops/releases/download/1.10.0/kops-linux-amd64
-chmod +x kops-linux-amd64
-sudo mv kops-linux-amd64 /usr/local/bin/kops
-kops get cluster
-kops export kubecfg --name ${KOPS_CLUSTER_NAME}
+# sudo snap install kubectl --classic
+# pip install awscli
+# wget https://github.com/kubernetes/kops/releases/download/1.10.0/kops-linux-amd64
+# chmod +x kops-linux-amd64
+# sudo mv kops-linux-amd64 /usr/local/bin/kops
+# kops get cluster
+# kops export kubecfg --name ${KOPS_CLUSTER_NAME}
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t phoenixking25/obscura-backend:latest -t phoenixking25/obscura-backend:$SHA .
-docker push phoenixking25/obscura-backend:latest
-docker push phoenixking25/obscura-backend:$SHA
+docker build -t warturtle/obscura6-backend:latest -t warturtle/obscura6-backend:$SHA .
+docker push warturtle/obscura6-backend:latest
+docker push warturtle/obscura6-backend:$SHA
 
-kubectl apply -f kubernetes
-kubectl set image deployment/backend-deployment server=phoenixking25/obscura-backend:$SHA
+# kubectl apply -f kubernetes
+# kubectl set image deployment/backend-deployment server=warturtle/obscura6-backend:$SHA
